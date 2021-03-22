d01ca3b83f4291e7844934f13b65fd4d40f7e14b
IvanCraft623 <57236932+IvanCraft623@users.noreply.github.com>
2021-03-21 22:00:24 -0500

    Create DeleteCommands.php

---
+++ src/IvanCraft623/DeleteCommands/DeleteCommands.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+#Plugin By:
+
+/*
+    8888888                            .d8888b.                   .d888 888     .d8888b.   .d8888b.   .d8888b.  
+      888                             d88P  Y88b                 d88P"  888    d88P  Y88b d88P  Y88b d88P  Y88b 
+      888                             888    888                 888    888    888               888      .d88P 
+      888  888  888  8888b.  88888b.  888        888d888 8888b.  888888 888888 888d888b.       .d88P     8888"  
+      888  888  888     "88b 888 "88b 888        888P"      "88b 888    888    888P "Y88b  .od888P"       "Y8b. 
+      888  Y88  88P .d888888 888  888 888    888 888    .d888888 888    888    888    888 d88P"      888    888 
+      888   Y8bd8P  888  888 888  888 Y88b  d88P 888    888  888 888    Y88b.  Y88b  d88P 888"       Y88b  d88P 
+    8888888  Y88P   "Y888888 888  888  "Y8888P"  888    "Y888888 888     "Y888  "Y8888P"  888888888   "Y8888P"  
+*/
+
+namespace IvanCraft623\DeleteCommands;
+
+use pocketmine\{Server, plugin\PluginBase, scheduler\ClosureTask};
+
+class DeleteCommands extends PluginBase {
+
+	public static $instance;
+
+	public function onLoad() : void {
+		self::$instance = $this;
+	}
+
+	public function onEnable() : void {
+		$this->saveResources();
+		$task = new ClosureTask(function(int $currentTick) : void {
+			$this->deleteCommands();
+		});
+		$this->getScheduler()->scheduleDelayedTask($task, 0);
+	}
+
+	public static function getInstance() : self {
+		return self::$instance;
+	}
+
+	public static function getConfigs(string $value) : config {
+		return new Config(self::getInstance()->getDataFolder() . "{$value}.yml", Config::YAML);
+	}
+
+	public function saveResources() : void {
+		$this->saveResource("config.yml");
+	}
+
+	public function deleteCommands() : void {
+		$commands = self::getConfig("config")->get("Commands");
+		foreach ($commands as $cmd) {
+			self::unregister($cmd);
+		}
+	}
+
+	public static function unregister(string $cmd) : bool {
+		$map = Server::getInstance()->getCommandMap();
+		$command = $map->getCommand($cmd);
+		if ($command !== null) {
+			$command->setLabel("old_".$cmd);
+			$map->unregister($command);
+			return true;
+		}
+		self::getInstance()->getLogger()->error('Could not delete "' . $cmd . '" command.');
+		return false;
+	}
+}
